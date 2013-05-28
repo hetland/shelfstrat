3dc3f9287525604457d79dd3f0d7539c53ba02b0
Rob Hetland <hetland@tamu.edu>
2013-05-28 09:07:32 -0500

    working version of run_case.py

--- run_case.py
+++ run_case.py
@@ -8,10 +8,12 @@ Copyright (c) 2007 Texas A&M Univsersity. All rights reserved.
 Release under MIT license.
 """
 
-from grd import make_grd
-# from analysis import make_xsec_movie
-
 import os
+
+from grd import make_grd
+from frc import make_frc
+from ini import make_ini
+
 
 class ROMS_in(object):
     """docstring for ROMS_IN"""
@@ -45,59 +47,55 @@ class ROMS_in(object):
         self.variables[key] = str(val)
 
 
-def run_case(rootdir='./project', 
-                    Uriver=0.1, 
-                    Utide=1.0,
-                    Href=10.0,
-                    Zob=0.001,
-                    shp=(30, 2, 256),
-                    Hmin=3.0, Hmax=15.0, 
-                    Lh=100e3, Le=300e3):
+def run_case(rootdir='./project',
+             M20=1e-6, N20=1e-4,
+             shp = (30, 128, 256)):
     
     # Make grid
     grdshp = (shp[1]+3, shp[2]+3)
-    make_grd(rootdir=rootdir, shp=grdshp, Hmin=Hmin, Hmax=Hmax, Le=Le, Lh=Lh)
+    make_grd(rootdir, Hmin=5.0, alpha=0.001, f=1e-4,
+             dx=1e3, dy=1e3, shp=grdshp)
+    
+    make_frc(rootdir, s_rho=shp[0], sustr0=0.0, svstr0=0.0, Tramp=3.0)
+    
+    make_ini(rootdir, s_rho=30, theta_s = 3.0, theta_b = 0.4, hc = 5.0,
+             Vtransform=1, Vstretching=1,
+             R0=1027.0, T0=25.0, S0=35.0, TCOEF=1.7e-4, SCOEF=7.6e-4,
+             M20=M20, M2_yo=50e3, M2_r=5e3,
+             N20=N20, N2_zo=50.0, N2_r=50.0)
     
     # run 3D case
-    rin_3d = ROMS_in('./project/External/coawst_estuary.in')
-    rin_3d['GRDNAME'] = os.path.join(rootdir, 'estuary_grd.nc')
-    rin_3d['FRCNAME'] = os.path.join(rootdir, 'estuary_frc.nc')
-    rin_3d['HISNAME'] = os.path.join(rootdir, 'estuary_his.nc')
-    rin_3d['AVGNAME'] = os.path.join(rootdir, 'estuary_avg.nc')
-    rin_3d['DIANAME'] = os.path.join(rootdir, 'estuary_dia.nc')
-    rin_3d['ININAME'] = './project/External/estuary_ini.nc'
-    rin_3d['RSTNAME'] = os.path.join(rootdir, 'estuary_rst.nc')
-    rin_3d['VARNAME'] = './project/External/varinfo.dat'
-    rin_3d['NUSER'] = '3'
-    rin_3d['USER'] = '%f %f %f' % (Uriver, Utide, Href)
+    rin_3d = ROMS_in('./project/ocean_shelfstrat.in')
+    rin_3d['GRDNAME'] = os.path.join(rootdir, 'shelfstrat_grd.nc')
+    rin_3d['FRCNAME'] = os.path.join(rootdir, 'shelfstrat_frc.nc')
+    rin_3d['HISNAME'] = os.path.join(rootdir, 'shelfstrat_his.nc')
+    rin_3d['AVGNAME'] = os.path.join(rootdir, 'shelfstrat_avg.nc')
+    rin_3d['DIANAME'] = os.path.join(rootdir, 'shelfstrat_dia.nc')
+    rin_3d['ININAME'] = os.path.join(rootdir, 'shelfstrat_ini.nc')
+    rin_3d['RSTNAME'] = os.path.join(rootdir, 'shelfstrat_rst.nc')
+    rin_3d['VARNAME'] = './project/varinfo.dat'
+
     rin_3d['Lm'] = shp[2]
     rin_3d['Mm'] = shp[1]
     rin_3d['N'] = shp[0]
     
-    dt = 30.0
-    rin_3d['NTIMES'] = 181400
-    rin_3d['DT'] = dt
-    rin_3d['NDTFAST'] = 10
-    
-    rin_3d['NHIS'] = int(86400 / dt / 24 / 4)
-    rin_3d['NDIA'] = int(86400 / dt / 24 / 4)
-    rin_3d['NDEFHIS'] = int(86400 / dt / 2)
-    rin_3d['NDEFDIA'] = int(86400 / dt / 2)
-    rin_3d['NAVG'] = int(86400 / dt / 2)
-    
-    rin_3d['Zob'] = 0.001
-    
-    infile = os.path.join(rootdir, 'ocean_estuary.in')
-    outfile = os.path.join(rootdir, 'ocean_estuary.out')
+    infile = os.path.join(rootdir, 'ocean_shelfstrat.in')
+    outfile = os.path.join(rootdir, 'ocean_shelfstrat.out')
     rin_3d.write(infile)
     print ' ### Running 3D ROMS...'
-    os.system('mpirun -np 8 ./project/coawstS < %s > %s' % (infile, outfile))
+    os.system('/opt/openmpi/gfortran/bin/mpirun -np 8 ./project/coawstM %s > %s' % (infile, outfile))
 
 
 if __name__ == '__main__':
     import argparse
     
     parser = argparse.ArgumentParser()
-    parser.add_argument('--foo', type=float, default=42.0, help='FOO!')
+    parser.add_argument('--M2', type=float, default=1e-6, 
+                        help='Horizontal stratification parameter (default=1e-6)')
+    parser.add_argument('--N2', type=float, default=1e-4, 
+                        help='Vertical stratification parameter (default=1e-6)')
     args = parser.parse_args()
-    print args
+    
+    rootdir = os.path.join('./simulations', 'shelfstrat_M2%4.2f_N2%4.2f' % (args.M2, args.N2))
+    
+    run_case(rootdir, M20=args.M2, N20=args.N2)
