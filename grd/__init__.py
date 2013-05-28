229c33f1bfe102628f242719084893ba03c8bf4c
Rob Hetland <hetland@tamu.edu>
2013-05-27 23:22:59 -0500

    Added __init__ files for frc, grd, and ini

---
+++ grd/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2.7
+# encoding: utf-8
+"""
+__init__.py
+
+Created by Rob Hetland on 2013-05-27
+Copyright (c) 2013 Texas A&M Univsersity. All rights reserved.
+Release under MIT license.
+"""
+
+from make_grd import make_grd
